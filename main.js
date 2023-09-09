1cf1918f3923fdf1419b6dec26315931cc59f623
KannKaro <111279140+KannKaro@users.noreply.github.com>
2023-09-09 15:39:47 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -4,8 +4,12 @@ let liczba;
 document.getElementById("Button1").onclick = function(nr1, nr2) {
     nr1 = document.getElementById("Nr1").value;
     nr2 = document.getElementById("Nr2").value;
-    
-    liczba = Math.floor(Math.random() * (nr2 - nr1) + nr1);
+    if(nr1>nr2) {
+        alert("blad");
+    }
+    else  {
+        liczba = Math.floor(Math.random() * (nr2 - nr1) + nr1);
 
-    document.getElementById("wynik").innerHTML = liczba;
+        document.getElementById("wynik").innerHTML = liczba;
+    }
 }
