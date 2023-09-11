cb9ecb2eaa8b082e9843ae64c8e8e8a24c25cb00
KannKaro <111279140+KannKaro@users.noreply.github.com>
2023-09-11 17:39:22 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 let nr1;
 let nr2;
 let liczba;
-document.getElementById("Button1").onclick = function(nr1, nr2) {
+document.getElementById("Button1").onclick = function (nr1, nr2) {
     nr1 = document.getElementById("Nr1").value;
     nr2 = document.getElementById("Nr2").value;
-    if(nr1>nr2) {
+    if (nr1 > nr2) {
         alert("blad");
     }
-    else  {
+    else {
         liczba = Math.floor(Math.random() * (nr2 - nr1) + nr1);
 
         document.getElementById("wynik").innerHTML = liczba;
